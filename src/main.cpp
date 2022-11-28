5c44081acbc3fc9e108ee41aa47190f0addc575e
hai <hai.nguyen@fuelcloud.com>
2022-11-28 17:06:08 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <ctime>
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 
 #include <nlohmann/json.hpp>
 
@@ -9,6 +11,8 @@ using json = nlohmann::json;
 
 auto main() -> int
 {
+  std::chrono::seconds interval(1);
+
   json j = json::parse(R"(
     {
       "pi": 3.141,
@@ -26,8 +30,10 @@ auto main() -> int
 
     std::cout << "Hello world!" << '\n';
     std::cout << "pi : " << j["pi"] << '\n';
-    std::cout << "foo: " << j["foo"] << '\n';
-    sleep(1);
+    std::cout << "foo: " << j["foo"] << '\n'
+              << '\n';
+
+    std::this_thread::sleep_for(interval);
   }
   return 0;
 }
