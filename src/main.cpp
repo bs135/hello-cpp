4826f47d733c6ec45442404c0f2368a30aac482f
hai <hai.nguyen@fuelcloud.com>
2022-12-07 13:36:21 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,47 @@ using json = nlohmann::json;
 
 void test_handler1(string topic, string msg)
 {
-  log_debug("message [{}]: {}", topic, msg);
+    log_debug("message [{}]: {}", topic, msg);
 }
 
 void test_handler2(string topic, string msg)
 {
-  log_debug("message [{}]: {}", topic, msg);
+    log_debug("message [{}]: {}", topic, msg);
 }
 
 void test_wildcard_handler(string topic, string msg)
 {
-  log_debug("message [{}]: {}", topic, msg);
+    log_debug("message [{}]: {}", topic, msg);
 }
 
 auto main() -> int
 {
-  app_env_init();
-  app_log_init();
+    app_env_init();
+    app_log_init();
 
-  /* test redis */
-  Redis_Init();
-  Redis_Subscribe("test/redisbus1", test_handler1);
-  Redis_Subscribe("test/redisbus2", test_handler2);
-  Redis_Subscribe("*/data", test_wildcard_handler);
-  Redis_Start();
+    /* test redis */
+    auto ok = Redis_Init();
+    if (ok)
+    {
+        Redis_Subscribe("test/redisbus1", test_handler1);
+        Redis_Subscribe("test/redisbus2", test_handler2);
+        Redis_Subscribe("*/data", test_wildcard_handler);
+        Redis_Start();
 
-  Redis_Client()
-      ->command("set", "abc", 321);
+        Redis_Client()
+            ->command("set", "abc", 321);
 
-  while (true)
-  {
-    log_info("Hello {}! {}", "world", 234);
-    Redis_Publish("test/redisbus3", "123");
-    std::this_thread::sleep_for(3s);
-  }
+        while (true)
+        {
+            log_info("Hello {}! {}", "world", 234);
+            Redis_Publish("test/redisbus3", "123");
+            std::this_thread::sleep_for(3s);
+        }
+    }
+    else
+    {
+        exit(EXIT_FAILURE);
+    }
 
-  return 0;
+    return 0;
 }
